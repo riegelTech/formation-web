5acdfee90a1b9798eab50a5a72dc395f8b842d91
baptiste_riegel <baptiste_riegel@croquetteland.com>
2023-03-24 14:28:09 +0100

    2023-03-24 PHP

--- src/index.php
+++ src/index.php
@@ -1,38 +1,72 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
-$props = ["nom", "espece", "age", "maladie", "activité"];
-$animalList = [
-	[$props[0] => "Toyant", $props[1] => "chat", $props[2] => 17, $props[3] => "Diabete", $props[4] => "calme"],
-	[$props[0] => "Dent", $props[1] => "chien", $props[2] => 7, $props[3] => "aucune", $props[4] => "magique"]
-];
+	$animalList = [
+		[
+			"ownerEmail" => "cbe@croquetteland.com", 
+			"nom" => "Toyant", 		
+			"espece" => "chat",  	
+			"infos" => [
+				"age" => 17, 	
+				"maladie" => "Diabete", 
+				"activity" => "calme"
+			]
+		],
+		[
+			"ownerEmail" => "cbe@croquetteland.com",
+			"nom" => "Dent",
+			"espece" => "chien",
+			"infos" => [
+				"age" => 7, 
+				"maladie" => "aucune", 
+				"activity" => "magique"
+			]
+		],
+		["ownerEmail" => "bri@croquetteland.com", 	"nom" => "Peau", 		"espece" => "chat",  	"infos" => ["age" => 12, 	"maladie" => "Schizophrenie", 	"activity" => "babyfoot"]],
+		["ownerEmail" => "bri@croquetteland.com", 	"nom" => "Chaud", 		"espece" => "chien",  	"infos" => ["age" => 7, 	"maladie" => "Alcoolique", 			"activity" => "pornfood"]]
+	];
 ?>  
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head></head>
 	<body>
 		<pre>
-		<?php
-		//print_r($animaux);
-		?>
+			<?php
+			// var_dump($animalList);
+			?>
 		</pre>
 		<table>
-			<tr>
+			<thead>
+				<tr>
+					<th>Nom</th>
+					<th>Email Propriétaire</th>
+					<th>Espèce</th>
+					<th>Infos</th>
+				</tr>
+			</thead>
+			<tbody> <!-- DM : Dynamiser le Tbody (les données sont dans la variable php $animalList -->
 				<?php
-				foreach ($props as $prop) {
-					echo "<th>$prop</th>";
-				}
-				?>
-			</tr>
-			<?php 
-			foreach( $animalList as $animal ) {
-			    echo "<tr>";
-			    foreach( $animal as $animalValue ) {
-			      echo "<td>$animalValue</td>";
-			    }
-			    echo "</tr>";
-			  }
-			?>
+				foreach ($animalList as $animal) { ?>
+					<tr>
+						<td>
+							<?php echo $animal["nom"]; ?>
+						</td>
+						<td>
+							<?php echo $animal["ownerEmail"]; ?>
+						</td>
+						<td>
+							<?php echo $animal["espece"]; ?>
+						</td>
+						<td>
+							<ul>
+								<?php foreach ($animal["infos"] as $nomInfo => $valeurInfo) { ?>
+									<li>
+										<?php echo ucfirst($nomInfo) . " : " . ucfirst($valeurInfo); ?>
+									</li>
+								<?php } ?>
+							</ul>
+						</td>
+					</tr>
+				<?php } ?>
+			</tbody>
 		</table>
 	</body>
 </html>
-
