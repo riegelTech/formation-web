146aee1f7110e599b0de9e4d38f1d871a4c7c568
baptiste_riegel <baptiste_riegel@croquetteland.com>
2023-04-24 18:10:06 +0200

    Result of the 3rd session

---
+++ src/animals/functions.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Récupération paginée des animaux de la base de données
+ * @param integer nombre de fiche par page
+ * @param integer numérotation de page
+ * @return array d'un tableau associatif par ligne animal (id/row)
+ */
+function getAnimals($nbAnimauxPage,$numeroPage) {
+        // On récupère la connexion à la base de données
+        global $conn;
+
+        // Récupération du nombre de fiches animal totales
+        // requete
+        $queryTotalFichesAnimal = "SELECT count(animal_id) AS total FROM fa.animal";
+
+        // Execution de la requete
+        $exect = mysqli_query($conn ,$queryTotalFichesAnimal);
+
+        // Recupération du tableau associatif
+        $fetch = mysqli_fetch_assoc($exect);
+
+        // Récupération total issue de l'execution de la requete
+        $total = $fetch['total'];
+
+        // Déterminer le premier élément de la page
+        $premierElementDeLaPage = ($numeroPage - 1) * $nbAnimauxPage;
+
+        // requete de la sélection des animaux paginée
+        $query = "SELECT * FROM fa.animal ORDER BY animal_id ASC LIMIT $premierElementDeLaPage, $nbAnimauxPage";
+
+        // initialisation du tableau de résultats
+        $result = array("total" => $total, "items" => []);
+
+        // execution de la requete
+        $result_bdd = mysqli_query($conn, $query);
+
+        // Récupération de la totalité des résultats de l'execution de la requete
+        while($row = mysqli_fetch_assoc($result_bdd)) {
+            $result["items"][] = $row;
+        }
+
+        // Retourne le tableau de résultats
+        return $result;
+}
+
+/**
+ * Recupere un Animal dans la base de données
+ * @param integer $id  ID de l'animal
+ * @return array tableau associatif representant l'animal
+ */
+function getAnimal($id) {
+         // On récupère la connexion à la base de données
+         global $conn;
+         // Requete
+        $queryInfoAnimal = "SELECT * FROM fa.animal WHERE animal_id = $id";
+        // Execution de la requette
+        $result = mysqli_query($conn ,$queryInfoAnimal);
+        // Récupération des résultats de la requete sous un tableau associatif
+        $fetch = mysqli_fetch_assoc($result);
+        // On retourne les résultats
+        return $fetch;
+}
+
+/**
+ * Sauvegarde un animal en base de données
+ * @param integer Identifiant de l'animal
+ * @param array tableau associatif representant l'animal
+ * @return boolean la bonne exécution (ou non) de la requete
+ */
+function updateAnimal($id, $animal){
+        // On récupère la connexion à la base de données
+        global $conn;
+        // Requete
+        $query = "UPDATE fa.animal
+        SET customer_id='"    . $animal['customer_id']     . "',
+            customer_email='" . $animal['customer_email']  . "',
+            espece_id="       . $animal['espece_id']       . ",
+            race_id="         . $animal['race_id']         . ",
+            stade_id="        . $animal['stade_id']        . ",
+            sousstade_id="    . $animal['sousstade_id']    . ",
+            date_naissance='" . $animal['date_naissance']  . "',
+            nom='"            . $animal['nom']             . "',
+            couleur='"        . $animal['couleur']         . "',
+            sexe='"           . $animal['sexe']            . "',
+            sterelise="       . $animal['sterelise']       . ",
+            activite='"       . $animal['activite']        . "',
+            pelage='"         . $animal['pelage']          . "',
+            poids_actuel='"   . $animal['poids_actuel']    . "',
+            poids_normal_theorique=" . $animal['poids_normal_theorique']. "
+        WHERE animal_id = $id";
+        // Execution de la requete
+        return mysqli_query($conn, $query);
+        // Fin
+}
+
+/**
+ * Supprimer un animal en base de données
+ * @param integer Identifiant de l'animal
+ * @return boolean la bonne exécution (ou non) de la requete
+ */
+function deleteAnimal($id) {
+        // On récupère la connexion à la base de données
+        global $conn;
+        // Requete
+        $query = "DELETE FROM fa.animal WHERE animal_id=$id";
+        // Execution de la requete
+        return mysqli_query($conn, $query);
+}
+?>
---
+++ src/animals/index.php
@@ -0,0 +1,64 @@
+<?php
+    // Se connecter à la base de données
+    include("../db_connect.php");
+    include("./functions.php");
+    $request_method = $_SERVER["REQUEST_METHOD"];
+
+    header('Content-Type: application/json');
+
+    function validator($paramName, $minValue, $defaultValue) {
+        if (isset($_GET[$paramName]) && is_numeric($_GET[$paramName]) && intval($_GET[$paramName], 10) > $minValue) {
+            return intval($_GET[$paramName], 10);
+
+        }
+        return $defaultValue;
+    }
+
+    switch($request_method) {
+
+        case 'GET':
+            // Si animal_id est renseigné alors c'est que l'on remonte 1 animal
+            $animalId = validator('animal_id', 1, null);
+            if ($animalId !== null) {
+                $animal = getAnimal($animalId);
+                echo json_encode($animal, JSON_PRETTY_PRINT);
+            }
+            // Sinon on affiche la liste des animaux
+            else {
+                $page = validator('page', 1, 1);
+                $pageSize = validator('pageSize', 1, 42);
+                $animaux = getAnimals($pageSize, $page);
+                echo json_encode($animaux, JSON_PRETTY_PRINT);
+            }
+            break;
+
+        case 'PUT':
+            $animalId = validator('animal_id', 1, null);
+            $animalToUpdate = json_decode(file_get_contents("php://input"), true);
+            $testUpdate = updateAnimal($animalId, $animalToUpdate);
+            if ($testAnimal === false) {
+                header("HTTP/1.0 500 Internal Server Error");
+            }
+
+            break;
+
+        case 'DELETE':
+            $animalId = validator('animal_id', 1, null);
+            if ($animalId !== null) {
+                $testDeleteAnimal = deleteAnimal($animalId);
+                if ($testDeleteAnimal === false) {
+                    header("HTTP/1.0 500 Internal Server Error");
+                }
+            }
+            else {
+                header("HTTP/1.0 400 Bad Request");
+                echo 'Tu vas mettre un ID sale con ?';
+            }
+            break;
+
+        default:
+            // Requête invalide
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+    }
+?>
